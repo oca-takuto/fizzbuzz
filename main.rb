8431a317529cda1fdf5a9f394c3816dae12b42f4
oca-takuto <2004240016ht@edu.oca.ac.jp>
2023-04-28 15:33:27 +0900

    comit

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 # main.rb
 require_relative './fizzbuzz'
 
-1.upto(100) do |n|
-    puts fizzbuzz(n)
+def check(input, expected)
+    actual = fizzbuzz(input)
+    unless actual == expected
+        abort "テストに失敗しました。\n入力値：#{input}, 期待する出力：#{expected}, 実際の出力：#{actual}"
+    end
 end
+
+check(1, 1)
+check(3, 'Fizz')
+check(5, 'Buzz')
+check(15, 'FizzBuzz')
